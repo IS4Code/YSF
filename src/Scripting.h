4062a1084c99d0d3598f16d0afa2e0658204a928
kurta999 <nmsstulaj@gmail.com>
2016-12-20 12:13:23 +0100

    Fixed a mistake

--- src/Scripting.h
+++ src/Scripting.h
@@ -325,7 +325,6 @@ namespace Natives
 	AMX_DECLARE_NATIVE(YSF_GetTickRate);
 	AMX_DECLARE_NATIVE(YSF_EnableNightVisionFix);
 	AMX_DECLARE_NATIVE(YSF_IsNightVisionFixEnabled);
-	AMX_DECLARE_NATIVE(YSF_EnableNightVisionFix);
 	AMX_DECLARE_NATIVE(YSF_ToggleOnServerMessage);
 	AMX_DECLARE_NATIVE(YSF_IsOnServerMessageEnabled);
 	AMX_DECLARE_NATIVE(YSF_SetExtendedNetStatsEnabled);
